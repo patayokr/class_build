87a34ed2212fb4d04710e3868b6c85a28e9fa1ef
nayuta <patayokr@naver.com>
2023-10-17 18:01:03 +0900

    분기 배포 -2

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,8 +10,8 @@ export default function BoardPage() {
   );
 }
 
-export const getServerSideProps = ()=>{
-//만약 서버사이드 랜더링을 하는 페이지라면 => out 폴더로 생성 불가
-//이런경우 next.confing.js에서 exportPathMap으로 export할 페이지에서 제외해야 한다
+// export const getServerSideProps = ()=>{
+// //만약 서버사이드 랜더링을 하는 페이지라면 => out 폴더로 생성 불가
+// //이런경우 next.confing.js에서 exportPathMap으로 export할 페이지에서 제외해야 한다
 
-}
+// }
